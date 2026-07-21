82214cd469c0a748f9104dea33b23a2063aa9a8c
naoki2610 <n_ishizawa1970@outlook.jp>
2026-07-21 11:38:04 +0900

    Add files via upload

---
+++ Positioning-check/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'survey-app-v1.0';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/proj4js/2.9.0/proj4.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // キャッシュにあればそれを返し、無ければネットワークから取得
+      return response || fetch(event.request);
+    })
+  );
+});
